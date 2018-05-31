b8e5b9ad8c0a905dc7b6872b7ee1665c7a7f8d96
Max Kaplan <max@avhana.com>
2018-05-31 13:22:49 -0400

    Remove skips

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -8,36 +8,36 @@ RSpec.describe Rover do
       expect(subject.position).to eq([0, 0])
     end
 
-    skip it 'returns :n for initial heading' do
+    it 'returns :n for initial heading' do
       expect(subject.heading).to eq(:n)
     end
 
-    skip it 'tracks multiple moves in the same direction' do
+    it 'tracks multiple moves in the same direction' do
       make_moves([[:n, 1], [:n, 1]])
       expect(subject.position).to eq([0, 2])
     end
 
-    skip it 'tracks the heading after each move' do
+    it 'tracks the heading after each move' do
       make_moves([[:n, 1], [:w, 1]])
       expect(subject.heading).to eq(:w)
     end
 
-    skip it 'tracks single moves of > 1 distance' do
+    it 'tracks single moves of > 1 distance' do
       make_moves([[:n, 3]])
       expect(subject.position).to eq([0, 3])
     end
 
-    skip it 'tracks moves in different directions' do
+    it 'tracks moves in different directions' do
       make_moves([[:w, 2], [:n, 1], [:s, 4]])
       expect(subject.position).to eq([-2, -3])
     end
 
-    skip it 'has a range of 10 on a single charge' do
+    it 'has a range of 10 on a single charge' do
       make_moves([[:n, 11]])
       expect(subject.position).to eq([0, 10])
     end
 
-    skip it 'has a range of 10 across n moves' do
+    it 'has a range of 10 across n moves' do
       make_moves([[:n, 4], [w: 6], [s: 2]])
       expect(subject.position).to eq([-6, 4])
     end
@@ -45,24 +45,24 @@ RSpec.describe Rover do
 end
 
 describe '#recharge' do
-  skip it 'has 0 charge after 10 moves' do
+  it 'has 0 charge after 10 moves' do
     make_moves([[:n, 10]])
     expect(subject.charge).to eq(0)
   end
 
-  skip it 'recharges from 0 to 10 with a wait time of 8 hr' do
+  it 'recharges from 0 to 10 with a wait time of 8 hr' do
     make_moves([[:n, 10]])
     charge(8)
     expect(subject.charge).to eq(10)
   end
 
-  skip it 'recharges from 0 to 5 with a wait time of 4 hr' do
+  it 'recharges from 0 to 5 with a wait time of 4 hr' do
     make_moves([[:n, 10]])
     charge(4)
     expect(subject.charge).to eq(5)
   end
 
-  skip it 'recharges from 0 to 2.5 with a wait time of 2 hr' do
+  it 'recharges from 0 to 2.5 with a wait time of 2 hr' do
     make_moves([[:n, 10]])
     charge(2)
     expect(subject.charge).to eq(2.5)
@@ -74,27 +74,27 @@ describe 'range and max_range' do
     { start_time: 0, hrs_of_light: 18 }
   end
 
-  skip it 'has a max range of 15 at dawn (0 hrs)' do
+  it 'has a max range of 15 at dawn (0 hrs)' do
     expect(subject.max_range).to eq(15)
   end
 
-  skip it 'has a range of 0 at 10 hours' do
+  it 'has a range of 0 at 10 hours' do
     make_moves([[:n, 10]])
     expect(subject.range).to eq(0)
   end
 
-  skip it 'has a max range of 5 at 10 hours' do
+  it 'has a max range of 5 at 10 hours' do
     make_moves([[:n, 10]])
     expect(subject.range).to eq(5)
   end
 
-  skip it 'as a range of 2 after charging fr 2 hours' do
+  it 'as a range of 2 after charging fr 2 hours' do
     make_moves([[:n, 10]])
     charge(2)
     expect(subject.range).to eq(2)
   end
 
-  skip it 'has a max_range of 10 if it starts at mid day, for 18hr of light' do
+  it 'has a max_range of 10 if it starts at mid day, for 18hr of light' do
     rover = Rover.new(start_time: 9, hrs_of_light: 18)
     expect(rover.max_range).to eq(10)
   end
@@ -102,7 +102,7 @@ end
 
 describe 'integration' do
   # note, the rover ignores move commands and recharge commands at night
-  skip it 'moves, charges, and moves until night' do
+  it 'moves, charges, and moves until night' do
     rover = Rover.new(start_time: 0, hrs_of_light: 18, day_length: 24)
     make_moves([[:w, 4], [:n, 1], [:s, 4], [:e, 1]]) # move 10
     charge(4)
